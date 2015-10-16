7fd90ebc477de92bae5418f3188d262d89af85df
Matthew Villwock <mtvillwock@gmail.com>
2015-10-15 17:56:59 -0700

    app clean

--- server.js
+++ server.js
@@ -14,14 +14,10 @@ app.use("/static", express.static("public"));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// DATA //
-var foods =[
-  {id: 0, name: "Sushiritto", yumminess: "quite"},
-  {id: 1, name: "Green Eggs & Ham", yumminess: "sure"},
-  {id: 2, name: "Crayfish", yumminess: "depending"},
-  {id: 3, name: "Foie Gras", yumminess: "omg"},
-  {id: 4, name: "Kale", yumminess: "meh"}
-];
+// Routes //
+app.get('/', function(req, res) {
+  res.render('index');
+})
 
 app.listen(3000, function (){
   console.log("listening on port 3000");
